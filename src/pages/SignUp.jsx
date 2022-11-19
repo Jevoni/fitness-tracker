c8e2a07e638eaf3478d582ad81360d5ca2e37ab2
Jevoni <jalenjay23@gmail.com>
2022-11-19 00:53:05 -0500

    added jwt decode and css styling

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 import { Box, Typography, Button } from '@mui/material'
 import AuthContext from '../context/AuthContext'
 
+import styles from './SignUp.module.css'
+
 const SignUp = () => {
     const { loginUser } = useContext(AuthContext)
     const [firstName, setFirstName] = useState('')
@@ -12,28 +14,10 @@ const SignUp = () => {
     const [confirmPassword, setConfirmPassword] = useState('')
 
     return (
-        <Box sx={{
-            display: 'flex',
-            width: '100%',
-            height: '100vh',
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: '#dbc3e4'
-        }}>
-            <Box sx={{
-                color: 'white',
-                height: '60%',
-                width: '35%',
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                backgroundColor: '#9b59b6',
-                padding: '20px',
-                borderRadius: '15px',
-                boxShadow: '0px 0px 3px 1px black',
-            }}>
-                <Typography variant='h5' sx={{ textAlign: 'center', marginBottom: '30px' }}>Fitness Tracker</Typography>
-                <form onSubmit={loginUser} style={{ display: 'flex', flexDirection: 'column', width: '70%' }}>
+        <Box className={`${styles['page']}`}>
+            <Box className={`${styles['signup-container']}`}>
+                <Typography variant='h5' className={`${styles['signup-container']}h5`}>Fitness Tracker</Typography>
+                <form onSubmit={loginUser} className={`${styles['signup-container']}form`}>
                     <input
                         type='text'
                         value={firstName}
@@ -69,9 +53,12 @@ const SignUp = () => {
                         autoComplete='new-password'
                         onChange={(e) => setConfirmPassword(e.target.value)}
                         style={{ height: '35px', fontSize: '15px', marginTop: '2px' }} />
-                    <Button variant='filled' type="submit" style={{ fontWeight: 'bold', color: 'black', backgroundColor: '#dbc3e4', marginTop: '20px', alignSelf: 'center', width: '30%', border: '1px solid black', textTransform: 'none' }}>Sign Up</Button>
+                    <Button variant='filled' type="submit" className={`${styles['login-container']}button`}>Sign Up</Button>
                 </form>
-                <Typography style={{ marginTop: '15px' }}>Have an account? <Link to='/' style={{ color: 'black', fontWeight: 'bold', textDecorationLine: 'none' }}>Log In</Link></Typography>
+                <Typography className={`${styles['signup-questions']}`}>
+                    Have an account?
+                    <Link to='/' className={`${styles['signup-questions']}a`}> Log In</Link>
+                </Typography>
             </Box>
         </Box>
     )
