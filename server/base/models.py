13277377bb7ec7986be933d37199599bbfba1626
Austin S <austins@austins-mbp.mynetworksettings.com>
2022-12-01 17:40:22 -0500

    new users can see their own posts, register new users, and posts are ordered by date

--- server/base/models.py
+++ server/base/models.py
@@ -10,8 +10,8 @@ class User(AbstractUser):
     # pass
     groups = None
     user_permissions = None
-    username = None
-    
+    username = models.CharField(max_length=40, unique=False, default='')
+
 
     email = models.EmailField(unique=True)
 
@@ -21,40 +21,40 @@ class User(AbstractUser):
 
 class Weight(models.Model):
 
-    user = models.ForeignKey(User,on_delete= models.CASCADE, default=1)
+    user = models.ForeignKey(User,on_delete= models.CASCADE)
     date = models.DateField()
     name = models.CharField(max_length=200)
     reps = models.IntegerField()
     sets = models.IntegerField()
 
     class meta:
-        ordering = ['-date', '-name']
+        ordering = ['date', '-name']
     
     def __str__(self):
         return self.name
 
 class Cardio(models.Model):
     
-    user = models.ForeignKey(User, on_delete= models.CASCADE, default=1)
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
     date = models.DateField()
     name = models.CharField(max_length=200)
     duration = models.DurationField()
 
     class meta:
-        ordering = ['-date', '-name']
+        ordering = ['date', 'name']
 
     def __str__(self):
         return self.name
 
 class Supplement(models.Model):
 
-    user = models.ForeignKey(User, on_delete= models.CASCADE, default=1)
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
     date = models.DateField()
     name = models.CharField(max_length=200)
     dossage = models.IntegerField()
 
     class meta:
-        ordering = ['-date', '-name']
+        ordering = ['date', 'name']
 
     def __str__(self):
         return self.name
