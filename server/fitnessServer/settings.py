b4bece26b3081da23aed2c60447e56f6917dacf5
Jevoni <jalenjay23@gmail.com>
2023-01-29 17:08:19 -0500

    Reverted

--- server/fitnessServer/settings.py
+++ server/fitnessServer/settings.py
@@ -12,10 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
-from decouple import config
 import os
-import dj_database_url
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'fitness-tracker',
+
     'base.apps.BaseConfig',
 
     'rest_framework',
@@ -98,7 +95,7 @@ MIDDLEWARE = [
     # added from django cors headers library
     'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import { BrowserRouter } from 'react-router-dom';
-import axios from 'axios'
-axios.defaults.xsrfCookieName = 'crsftoken'
-axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
